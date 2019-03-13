fff0786e0e90ce7e1cc249c06e128dbde8fbe622
Ján Sliacky <register4marketing@gmail.com>
2019-03-13 19:50:03 +0100

    Vymazany script.js

--- gulp/tasks/scripts.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var webpack = require('webpack');
-
-gulp.task('scripts', function(callback){
-	webpack(require('../../webpack.config.js'), function() {
-		console.log('works');
-		callback();
-	}); 	
-});
