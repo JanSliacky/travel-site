f4de530638c5615d784eb0fb804f7ae6665ae446
Ján Sliacky <register4marketing@gmail.com>
2019-03-16 20:18:36 +0100

    Rozbehnute fungovanie jquery

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,16 +1,3 @@
-// var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " works");
-	}
-}
-
-
-var john = new Person('John Doe', 'piss');
-john.greet();
-
-var jane = new Adult('Jane Jaden', 'fuck');
-jane.greet();
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
